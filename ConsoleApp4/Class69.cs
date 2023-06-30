3befd6d0539e89f6ab6527a27a8d3c3db592727b
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-06-30 19:38:15 +0530

    friday

---
+++ ConsoleApp4/Class69.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Mymath
+    {
+        protected double result;
+        public  double get()
+        {
+          return result;        
+        }
+    }
+    class Mymath2 : Mymath 
+    {
+        public void  Set(int a,int b)
+        {
+            result = a + b;
+
+        }
+    }
+    class Mymath3 : Mymath
+    {
+        public void set(int a,int b)
+        {
+            result = a - b;
+        }
+    }
+
+    internal class Class69
+    {
+        static void Main()
+        {
+            Mymath2 m1= new Mymath2();
+            m1.Set(1,2);
+            Mymath3 m2= new Mymath3();
+            m2.set(1,2);
+            Console.WriteLine( "add : "+m1.get());
+            Console.WriteLine("sub : "+m2.get());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp4/Class70.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class area
+    {
+        protected double result;
+        public double geeet()
+        {
+            return result;
+        }
+
+    }
+    class reactangle : area
+    {
+        public void set(int l,int b)
+        {
+            result = l * b;
+        }
+    }
+    class circle : area
+    {
+        public void set(int r)
+        {
+            result= r * r*Math.PI;
+        }
+    }
+    internal class Class70
+    {
+        static void Main()
+        {
+          circle c= new circle();
+            c.set(1);
+            reactangle r = new reactangle();
+            r.set(23,5);
+            Console.WriteLine(c.geeet());
+            Console.WriteLine(r.geeet());
+            Console.ReadKey();
+        }
+    }
+}
